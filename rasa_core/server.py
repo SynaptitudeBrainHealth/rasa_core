45a5a0553a4174d75cb3475293505b142bfbf9c6
Kirti Agarwal <kirti@synaptitude.info>
2019-09-19 14:36:36 -0700

    add logger statements

--- rasa_core/server.py
+++ rasa_core/server.py
@@ -723,8 +723,10 @@ def _get_output_channel(
         requested_output_channel = tracker.get_latest_input_channel()
 
     # Interactive training does not set `input_channels`, hence we have to be cautious
+
     # registered_input_channels = getattr(request.app, "input_channels", None) or []
-    registered_input_channels = [RestInput, SlackInput]
+
+    registered_input_channels = [RestInput(), SlackInput()]
     logger.info('registered_input_channels: {}'.format(registered_input_channels))
     matching_channels = [
         channel
